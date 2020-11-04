296eacf9f271ffe28c8c254a08a959f7e909b25e
mcflyhalf <mcflyhalf@live.com>
2020-11-04 21:52:40 +0400

    Include building V0.1
    Initial implementation to demonstarte to students how basic tasks can be done int the building. Implementation incomplete

--- Building.java
+++ Building.java
@@ -1,11 +1,41 @@
 public class Building {
 	//Data
+	private int ROOMS_LONG;
+	private int ROOMS_WIDE;
 
-	//cop_position
-	//robber_position
-
+	private int[] cop_position = new int[2];
+	private int[] robber_position = new int[2];
 
 	//Methods
+	public Building(int rooms_wide, int rooms_long) {
+		this.ROOMS_WIDE = rooms_wide;
+		this.ROOMS_LONG = rooms_long;
+
+		//TODO: Use setter (moveCharacter) to set these
+		this.cop_position[0] = 0;
+        this.cop_position[1] = 0;
+        this.robber_position[0] = ROOMS_WIDE-1;
+        this.robber_position[1] = ROOMS_LONG-1;
+
+		/*ROOM LAYOUT when ROOMS_WIDE = 6, ROOMS_LONG = 4
+		 _ _ _ _ _ _ _ _ _ _ _ _
+		|   |   |   |   |   |   |
+		|0,3|1,3|2,3|3,3|4,3|5,3|		
+		| - - - - - - - - - - - |
+		|   |   |   |   |   |   |
+		|0,2|1,2|2,2|3,2|4,2|5,2|
+		| - - - - - - - - - - - |	LENGTH
+		|   |   |   |   |   |   |
+		|0,1|1,1|2,1|3,1|4,1|5,1|
+		| - - - - - - - - - - - |
+		|   |   |   |   |   |   |
+		|0,0|1,0|2,0|3,0|4,0|5,0|
+		|- - - - - - - - - - - -|
+				WIDTH
+		*/
+
+
+	}	
 
 	/* moveCharacter
 	 * Moves the specified character in the specified direction 
@@ -20,15 +50,66 @@ public class Building {
 		else
 			move the robber
 		 */
+		int[] characterToMove;
+		if(cop){
+			characterToMove = cop_position; 
+		} else {
+			characterToMove = robber_position;
+		}
+
+		if (direction == 0){
+			//Stay where you are
+			return true;
+		}
+		else if(direction == 1) { 
+			if (characterToMove[1] > ROOMS_LONG - 2) {
+				//if we are on the Northern edge
+				return false;
+			}
+			else {
+				characterToMove[1] += 1;
+				return true;
+			}
+
+		}
+		else if(direction == 2) { 
+			
+		}
+		else if(direction == 3) { 
+			
+		}
+		else if(direction == 4) { 
+			
+		}
+		else if(direction == 5) { 
+			
+		}
+		else {
+			return false;
+		}
+
+		return false;
 	}
 
 	//Getter that returns cop or robber pos
-	int[][] getCharacterPos (Boolean cop) {
+	int[] getCharacterPos (Boolean cop) {
 		/*if cop
 			return cop's position
 		else
 			return robber's position
 		 */
+		int[] characterToGet;
+		if(cop){
+			characterToGet = cop_position; 
+		} else {
+			characterToGet = robber_position;
+		}
+
+		//Clone method ensures we only pass a copy of the cop/robber position
+		//So it cant be editted
+		int[] position = characterToGet.clone();
+
+		return position;
 	}
 
 
