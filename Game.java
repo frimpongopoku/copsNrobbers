5a6558eb34dc23d9fc1ad66c6cd69a036684284a
Mcflyhalf <mcflyhalf@live.com>
2020-11-10 08:31:05 +0400

    Dev game development - Add meat to skeleton (#1)
    * Include building V0.1
    
    Initial implementation to demonstarte to students how basic tasks can be done int the building. Implementation incomplete
    
    * Complete starter code
    
    Include starter code for Character and Game Class to give hints about the general flow of the program.
    
    Retain bluej package file for helping students. Include .gitignore

--- Game.java
+++ Game.java
@@ -1,5 +1,11 @@
+import java.util.concurrent.TimeUnit;
+
 public class Game {
 	//data
+	Building building;
+	Character player;
+	Character ai; 
+
 	int MAX_TURNS;
 	int turns_taken;
 
@@ -13,15 +19,64 @@ public class Game {
 	* Places the cop and robber in appropriate room(s)
 	* Sets MAX_TURNS - time that cop has to catch robber
 	*/
-	public Game(int roomswide, int roomslong, Boolean isCop, ) {
+	public Game(int roomswide, int roomslong, Boolean isCop) {
 		//Game Constructor - Do setup work here
+		String robberName = "Mandem the Terrible";
+		this.building = new Building(roomswide, roomslong);
+		Character cop = new Character(true);
+		Character robber = new Character(robberName, false); 
+
+		this.player = cop;
+		this.ai = robber; 
+
+		this.turns_taken = 0;
+	}
+
+	public static void pause(int secondsToSleep) {
+		try {
+	    TimeUnit.SECONDS.sleep(secondsToSleep);
+		} 
+	catch (InterruptedException ie) {
+	    Thread.currentThread().interrupt();
+		}
 	}
 
 
 	public static void main(String[] args) {
-		//Print some startup messages
+	
+	int ROOMS_WIDE = 4;
+	int ROOMS_LONG = 5;
+
+
+	// ---SETTING UP THE GAME ---\\
+		//Print some startup messages to look fancy
+		System.out.print("Initialising game\t");
+		int wait_time = 5;
+
+		for (int cnt=0;cnt<wait_time;cnt++) {
+			System.out.print(". ");
+			pause(1);
+		}
 
 		//Create a new game (constructor)
+		Game game = new Game(ROOMS_WIDE, ROOMS_LONG, true);
+	
+
+	// ---ACTUAL GAMEPLAY HAPPENS HERE ---\\
+		while (!game.gameOver()) {
+			game.turn();
+
+			System.out.println("Only one turn can currently be taken. Goodbye!");
+			break;
+		}
+
+		System.out.print("Shutting Down\t");
+
+		for (int cnt=0;cnt<wait_time;cnt++) {
+			System.out.print(". ");
+			pause(1);
+		}
+		System.out.print("\n\n\t\tThanks for playing copsNrobbers!!!");
 
 		//Game loop (The things that happen in the actual game, these happen in an infinite loop)
 			/* Allow Cop to take a turn
@@ -32,14 +87,15 @@ public class Game {
 	}
 
 	/* gameOver
-	 * Checks for game over conditions. These are (students need to figure these out):
+	 * Checks for game over conditions.  These are (students need to figure these out):
 	 * 1. 
 	 * 2. 
 	 * 3. 
+	 * Should return true if game should end and false otherwise
 	 * It should also print the appropriate game over message
 	 */
 	public Boolean gameOver() {
-
+		return false;
 	}
 
 	/*turn
@@ -47,6 +103,17 @@ public class Game {
 	 *
 	 */
 	public void turn() {
+		//playerMove will be used to record the direction player wanted to move
+		int playerMove;
+
+		//cop takes his turn
+		playerMove = player.takeTurn();
+
+		//Robber takes his turn
+		this.robberTurn(); 
+
+		//increment turn count
+		this.turns_taken += 1;
 
 	}
 
@@ -57,6 +124,11 @@ public class Game {
 	 * This method will be called by Game.turn()
 	 */
 	public void robberTurn () {
+		// For now, this will be empty
+		// so the robber does nothing and
+		// the cop can easily catch the robber
+		System.out.println("The Robber has taken his turn\n\n");
+		pause(3);
 
 	}
 
